f2e2814afbe0a0049ad2b3f60eea1e4763700663
Priya <pghosh8825@gmail.com>
2026-04-28 21:29:38 +0530

    Include notebook to explain project steps

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import Olympic_Preprocessor
 import helper
 from helper import medal_tally
+st.set_page_config(layout="wide")
 
 df = pd.read_csv('athlete_events.csv')
 region_df = pd.read_csv('noc_regions.csv')
@@ -21,6 +22,16 @@ if user_menu == 'Medal Tally':
 
     selected_year = st.sidebar.selectbox("select Year",years)
     selected_country = st.sidebar.selectbox("select Country", country)
-    medal_tally = helper.medal_tally(df)
-    st.dataframe(medal_tally)
+
+    medal_tally = helper.fetch_medal_tally(df,selected_year,selected_country)
+    if selected_year == 'Overall' and selected_country == 'Overall':
+        st.title("Overall Tally")
+    if selected_year != 'Overall' and selected_country == 'Overall':
+        st.title("Medal TAlly in "+str(selected_year)+" olympics")
+    if selected_year == 'Overall' and selected_country != 'Overall':
+        st.title(selected_country+" Overall Performance")
+    if selected_year != 'Overall' and selected_country != 'Overall':
+        st.title(f" {selected_country} performance in {selected_year}")
+    st.table(medal_tally)
+
 
